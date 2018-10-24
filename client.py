296e7cefe2a49a9c4308b01f2dace0f986868571
Claudia Alvarez Bravo <claualbr@beta17.aulas.gsyc.urjc.es>
2018-10-24 10:54:16 +0200

    escribir desde el shell el mensaje del cliente

--- client.py
+++ client.py
@@ -5,14 +5,18 @@ Programa cliente UDP que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Constantes. Dirección IP del servidor y contenido a enviar
 SERVER = 'localhost'
-PORT = 6001
-LINE = '¡Hola mundo!'
+PORT = int(sys.argv[2]) #mas de 1024
+LINE = ' '.join(sys.argv[3:])
+IP = sys.argv[1]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+# (socket internet,tipo de socket udp) como llamamos a socket
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+    print(PORT)
     my_socket.connect((SERVER, PORT))
     print("Enviando:", LINE)
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
