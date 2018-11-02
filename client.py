f119b18881e0731d02db595cc9e244e584b84a81
Claudia Alvarez Bravo <claualbr@alpha21.aulas.gsyc.urjc.es>
2018-11-02 13:02:34 +0100

    pep8 + pep257

--- client.py
+++ client.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Programa cliente UDP que abre un socket a un servidor
-"""
-
+"""Programa cliente UDP que abre un socket a un servidor."""
 import socket
 import sys
-
-# Constantes. Dirección IP del servidor y contenido a enviar
+# Constantes. Dirección IP del servidor, puerto, clase de petcion,
+# direccion y tiemp de expiracion
 try:
     SERVER = sys.argv[1]
     PORT = int(sys.argv[2])
@@ -17,13 +14,15 @@ try:
 except IndexError:
     sys.exit("Usage: client.py ip puerto register sip_address expires_value")
 
+
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", PETICION.upper() + ' sip:'+ ADRESSS + ' SIP/2.0')
+    print("Enviando:", PETICION.upper() + ' sip:' + ADRESSS + ' SIP/2.0')
     print("Expires: " + EXPIRES)
-    my_socket.send(bytes(PETICION.upper() + ' sip:'+ ADRESSS + ' SIP/2.0\r\n '
-                        + EXPIRES, 'utf-8') + b'\r\n\r\n')
+    # Enviamos al servidor nuestra peticion
+    my_socket.send(bytes(PETICION.upper() + ' sip:' + ADRESSS + ' SIP/2.0\r\n '
+                         + EXPIRES, 'utf-8') + b'\r\n\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
--- server.py
+++ server.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Clase (y programa principal) para un servidor de eco en UDP simple
-"""
+"""Clase (y programa principal) para un servidor de eco en UDP simple."""
 
 import socketserver
 import sys
@@ -11,30 +9,33 @@ import json
 
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
-    """
-    Echo server class
-    """
+    """Inicializo el diccionario de usuarios."""
+
     dicc = {}
 
     def json2register(self):
+        """Descargo fichero json en el diccionario."""
         try:
             with open('registered.json', 'r') as jsonfile:
                 self.dicc = json.load(jsonfile)
-        except FileNotFoundError:
+        except:
             pass
 
     def register2json(self):
         """
-        Escribir diccionario en formato json en el fichero registered.json
+        Escribir diccionario.
+
+        En formato json en elfichero registered.json.
         """
         with open('registered.json', 'w') as jsonfile:
             json.dump(self.dicc, jsonfile, indent=4)
 
-
     def handle(self):
         """
-        handle method of the server class
-        (all requests will be handled by this method)
+        Recibo el mensaje del cliente.
+
+        En el caso de que la peticion sea un
+        register guardo la informacion en un diccionario.
         """
         self.json2register()
         self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
@@ -47,17 +48,20 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             if peticion == 'REGISTER':
                 IP = self.client_address[0]
                 user = address.split(':')[1]
+                # Timpo actual mas expiracion
                 Time = time.time() + int(expires)
                 TimeExp = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(Time))
                 if int(expires) != 0:
-                    self.dicc[user] = {'address': IP,'expires': TimeExp}
+                    self.dicc[user] = {'address': IP, 'expires': TimeExp}
                 elif int(expires) == 0:
+                    # Si el tiempo de expiracion es o se borra el diccionario
                     try:
                         del self.dicc[user]
                     except KeyError:
                         pass
                 self.register2json()
                 print(self.dicc)
+
 
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001
